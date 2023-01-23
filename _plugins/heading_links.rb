c825fbb2866737a4ddf6bb256205fda5b7090b1d
Greg Walker <greg.walker@gsa.gov>
2023-01-23 11:23:54 -0600

    heading links

---
+++ _plugins/heading_links.rb
@@ -0,0 +1,5 @@
+Jekyll::Hooks.register :documents, :post_render do |doc|
+  if doc.output_ext == ".html"
+    doc.output = doc.output.gsub(/<h[1-6].*?>.*?<\/h[1-6]>/, '<a href="#" class="heading">\0</a>')
+  end
+end

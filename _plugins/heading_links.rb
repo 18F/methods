ab5e75c5dd7f4d0aa88052e8fbe1f0d6b81b0396
Greg Walker <greg.walker@gsa.gov>
2023-01-23 12:10:01 -0600

    is it running?

--- _plugins/heading_links.rb
+++ _plugins/heading_links.rb
@@ -1,5 +1,5 @@
 Jekyll::Hooks.register :documents, :post_render do |doc|
   if doc.output_ext == ".html"
-    doc.output = doc.output.gsub(/<h[1-6].*?>.*?<\/h[1-6]>/m, '<a href="#" class="heading">\0</a>')
+    doc.output = "<!-- HOWDY -->" + doc.output.gsub(/<h[1-6].*?>.*?<\/h[1-6]>/m, '<a href="#" class="heading">\0</a>')
   end
 end
